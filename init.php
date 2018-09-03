ba94d8f4e92a4cce10de5d622a90218570d35120
Sean Kelleher <ezanmoto@gmail.com>
2018-09-03 16:36:30 +0000

    Tidied up `init.php`

--- init.php
+++ init.php
@@ -1,12 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "toor";
-
 $mysql_conn = new mysqli('localhost', 'user', 'pass', 'database');
 if ($mysql_conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        die("Connection failed: " . $mysql_conn->connect_error);
 } 
 
-$trustapHost = "https://stagedemo.trustap.com";
+$trustapHost = "https://stageapp.trustap.com";
 ?>
